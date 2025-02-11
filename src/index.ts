82025344da6fcf0eae4fc689cd851165f6e69226
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-02-11 13:41:57 -0800

    consolidate files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { EventEmitter } from "node:events"
+import { DurableObject } from "cloudflare:workers"
 import { generateLogSegmentName, parseLogSegmentName, SegmentMetadata } from "./segment"
-import { SegmentIndex } from "./segment_index"
+import { RBTree } from "bintrees"
 
 const FlushIntervalMs = 200
 
 const consumerOffsetKeyPrefix = "consumer_offset::"
+
+const hour = 1000 * 60 * 60
+const day = hour * 24
+
+const activeLogSegmentKey = "active_log_segment::" // what logs segments are actually active, used for compaction, tombstone cleaning, and queries
+const MaxStaleSegmentMs = day * 1 // 1 day
+
+function buildLogSegmentIndexKey(segmentName: string): string {
+	return `${activeLogSegmentKey}${segmentName}`
+}
 
 interface PendingMessage {
 	emitter: EventEmitter<{ resolve: [string[]]; error: [Error] }>
@@ -29,7 +40,7 @@ function serializeOffset(epoch: number, counter: number): string {
 	return `${epoch.toString().padStart(16, "0")}${counter.toString().padStart(16, "0")}`
 }
 
-export class StreamCoordinator extends SegmentIndex<Env> {
+export class StreamCoordinator extends DurableObject<Env> {
 	connectedWebsockets: number = 0
 	consumers: Map<WebSocket, string> = new Map()
 	consumerOffsets: Map<string, string> = new Map()
@@ -39,6 +50,8 @@ export class StreamCoordinator extends SegmentIndex<Env> {
 	epoch: number = Date.now()
 	counter: number = 0
 
+	tree: RBTree<SegmentMetadata>
+
 	// Messages that are pending persistence in the flush interval
 	pendingMessages: PendingMessage[] = []
 
@@ -47,6 +60,28 @@ export class StreamCoordinator extends SegmentIndex<Env> {
 
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env)
+
+		// We know 2 things to be true about SegmentMetadata:
+		// 1. firstOffset is always less than or equal to lastOffset
+		// 2. No instances SegmentMetadata can have intersecting offset ranges
+		// Therefore we can make decisions entirely off of the first offset
+		this.tree = new RBTree<SegmentMetadata>((a, b) => {
+			// Return 0 if a == b
+			// > 0 if a>b
+			// < 0 if a<b
+			if (a.firstOffset < b.firstOffset) {
+				// a is before b
+				return -1
+			}
+			if (a.firstOffset > b.firstOffset) {
+				// a is after b
+				return 1
+			}
+
+			// should we crash here?
+			console.error("2 segments with intersecting offset ranges - this is a consistency bug", a, b)
+			return 0
+		})
 	}
 
 	finishSetup() {
@@ -248,6 +283,23 @@ export class StreamCoordinator extends SegmentIndex<Env> {
 
 		await Promise.all([writer.close(), writePromise])
 		console.log(`Wrote ${records} records to ${segmentName}`)
+	}
+
+	async buildIndexFromStorage() {
+		const segments = await this.ctx.storage.list<SegmentMetadata>({
+			prefix: activeLogSegmentKey,
+		})
+
+		for (const [_, segment] of segments) {
+			this.tree.insert(segment)
+		}
+	}
+
+	async writeLogSegmentMetadata(metadata: SegmentMetadata) {
+		// First we need to durably store it
+		await this.ctx.storage.put(buildLogSegmentIndexKey(metadata.name), JSON.stringify(metadata))
+		// Then we can add it to the memory index
+		this.tree.insert(metadata)
 	}
 
 	async compactLogSegments(segments: SegmentMetadata[]) {
